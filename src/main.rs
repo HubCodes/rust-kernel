14fcbbf357daa102fdc2312519a8598f3bf1e024
Taemin Cha <krlrhkstk12@gmail.com>
2021-05-03 21:21:33 +0900

    Add environment for testing

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,58 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![feature(const_fn_trait_bound)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
 
 use core::panic::PanicInfo;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failure = 0x11,
+}
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello, world!");
 
+    #[cfg(test)]
+    test_main();
+
     loop {}
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     println!("{}", _info);
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[& dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
